d06f2d3b197ed1acbf1ec01f981123f1d71b40eb
Sergey Smirnov <smirnov.sege@gmail.com>
2023-05-27 10:56:44 +0300

    main font changed to Open Sans

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import { Open_Sans } from 'next/font/google'
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Open_Sans({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
